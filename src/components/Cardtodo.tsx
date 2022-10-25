05f8005b28bac027c99d6bf558b844c2701c0cb4
ahmad.mohammadi <a.mohammadi1026@gmail.com>
2022-10-25 09:13:44 +0330

    add tailwindcss

---
+++ src/components/Cardtodo.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const CardTodo:React.FC = ()=>{
+    return(
+        <div className='w-96 h-96 border'>
+            
+        </div>
+
+    );
+}
+
+
+export default CardTodo
